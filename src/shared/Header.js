9e02a4ac103f80318d1f56c1d7c076e9ed543fe3
Paakraad <paakraad@Adels-MBP-2.attlocal.net>
2022-12-29 19:55:05 -0800

    styling for nav menu

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,46 +1,69 @@
 import React from 'react';
-import "./header.css";
-import Logo from './../assests/design-imgs/logo-1.png'
-import { useRef } from 'react';
-import ScrollToTop from '../hooks/ScrollToTop';
-import { MdArrowDropDownCircle } from 'react-icons/md';
+import Logo from './../assests/design-imgs/logo-1.png';
+import styled from 'styled-components';
+
+
+
+
+
+
 
 export default function Header({handleClick}) {
-  // const [isNavExpanded, setIsNavExpanded] = useState(false);
-
-	// const scrollDown = (ref) => {
-	// 	window.scrollTo({
-	// 		top: ref.current.offsetTop,
-	// 		behavior: 'smooth',
-	// 	});
-	// };
-
-
 
   return (
-    <nav className="navigation">
-      <a href="/" className="brand-name">
-        <img className="logo" src={Logo} alt="logo" />
+    <Nav>
+      <a href="/">
+        <Img src={Logo} alt="logo" />
       </a>
-      <div className="#">
-				<ul className="nav-links">
-					{/* <li className="link" onClick={() => scrollDown(aboutSection)}> */}
-					<li className="link" onClick={() => handleClick(1)}>
+      <Links>
+				<Ul>
+					<Li className="link" onClick={() => handleClick(1)}>
 						Services
-					</li>
-					<li className="link" onClick={() => handleClick(2)}>
+					</Li>
+					<Li className="link" onClick={() => handleClick(2)}>
 						Gallery
-					</li>
-					<li className="link" onClick={() => handleClick(3)}>
+					</Li>
+					<Li className="link" onClick={() => handleClick(3)}>
 						Contact
-					</li>
-				</ul>
-			</div>
-      {/* <botton>
-        <a href="#">food</a>
-        <a href="services">services</a>
-        <a href="#">Gallery</a>
-      </botton> */}
-    </nav>
+					</Li>
+				</Ul>
+			</Links>
+    </Nav>
   );
 }
+
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+    height: 60px;
+    width: 100%;
+    padding: 0.5rem 0rem;
+    background-color: #fddfd7;
+`
+
+const Img = styled.img`
+  width: 8rem;
+  margin: 1rem;
+`
+
+const Links = styled.div`
+  
+`
+const Ul = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`
+
+const Li = styled.li`
+  list-style: none;
+  margin: 1rem;
+  cursor: pointer;
+  font-family: 'Karantina', cursive;
+  font-size: 1.7rem;
+  color: #707070;
+
+`
