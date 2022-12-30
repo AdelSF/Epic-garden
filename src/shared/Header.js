6e7cef489970b7369cabc68d1b1a203e6915f060
Paakraad <paakraad@Adels-MBP-2.attlocal.net>
2022-12-30 09:44:18 -0800

    styling hamberger menu

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -40,120 +40,61 @@ export default function Header({handleClick}) {
   );
 }
 
-
-// const Nav = styled.nav`
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-//   align-items: center;
-//   height: 60px;
-//   width: 100%;
-//   padding: 0.5rem 0rem;
-//   background-color: #fddfd7;
-// `
-
 const Img = styled.img`
   width: 8rem;
   margin: 1rem;
 `
 
-// const HamMenu = styled.div`
-
-// `
-
-// const MoreDetails = styled.div`
-//   padding: 2rem;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-//   flex-wrap: wrap;
-//   width: 95%;
-//   background: #ffffff;
-//   border: 2px solid #FC5600;
-//   border-top: none;
-//   text-align: left;
-//   @media only screen and (max-width: 600px) {
-//       width: 95%;
-//   }
-// `
-
-// const Links = styled.div`
-  
-// `
-// const Ul = styled.ul`
-//   display: flex;
-//   justify-content: space-between;
-//   flex-direction: column;
-// `
-
-// const Li = styled.li`
-//   list-style: none;
-//   /* margin: 1rem; */
-//   cursor: pointer;
-//   font-family: 'Karantina', cursive;
-//   font-size: 1.7rem;
-//   color: #707070;
-
-// `
-
-
-
 const Menu = styled.header`
-    display: flex;
-    justify-content: space-between;
-    /* border-bottom: 1px solid gray; */
-    background-color: #fddfd7;
-
-    /* background-color: black; */
+  display: flex;
+  justify-content: space-between;
+  background-color: #fddfd7;  
 `
 
-// const Logo = styled.img`
-//     height: 40px;
-//     margin-left: 1.5rem;
-//     @media only screen and (max-width: 600px) {
-//     margin: 3%;
-//   }
-// `
-
 const Items = styled.nav`
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 400px;
-    margin-right: 1.5rem;
-    font-family: 'Karantina', cursive;
-    font-size: 1.5rem;
-    color: #707070;
-    letter-spacing: 1px;
-    @media (max-width: 600px) {
-        display: ${({displayStatus}) => displayStatus === '╳' ? 'flex' : 'none' };
-        flex-direction: column;
-        height: 30vh;
-        margin-top: 10vh;
-        width: unset;
-    }
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 400px;
+  margin-right: .5rem;
+  font-family: 'Karantina', cursive;
+  font-size: 1.5rem;
+  letter-spacing: 1px;
+  @media (max-width: 600px) {
+    display: ${({displayStatus}) => displayStatus === '╳' ? 'flex' : 'none' };
+    flex-direction: column;
+    height: 30vh;
+    margin-top: 2vh;
+    width: unset;
+    position: absolute;
+    right: 0;
+    top: 2rem;
+  }
 `
 
 const Item = styled.div`
-    text-decoration: none;
-    font-weight: 500;
-    cursor: pointer;
-
-    &:hover {
-        text-shadow: 0px 0px 5px black;
-        color: white;
-        transition: .7s;
-    }
-    @media only screen and (max-width: 600px) {
-    margin-bottom: 50%;
+  text-decoration: none;
+  font-weight: 500;
+  padding: 5px;
+  cursor: pointer;
+  width: 60px;
+  &:hover {
+      text-shadow: 0px 0px 5px black;
+      color: white;
+      transition: .7s;
+  }
+  @media only screen and (max-width: 600px) {
+    background-color: #feece7;
+    border: 1px solid gray;
+    border-radius: 5px;
   }
 `
 
 const Ham = styled.span`
-    color: black;
-    font-size: 2rem;
-    margin: 10px 20px;
-    @media (min-width: 600px) {
-        display: none;
-    }
+  color: black;
+  font-size: 2rem;
+  margin: 10px 20px;
+  @media (min-width: 600px) {
+      display: none;
+  }
 `
