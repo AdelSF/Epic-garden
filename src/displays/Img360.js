e424daf17fe255b44a906888a5258d6cc666904b
Paakraad <paakraad@Adels-MBP-2.attlocal.net>
2023-01-23 11:48:12 -0800

    360 image styling

--- src/displays/Img360.js
+++ src/displays/Img360.js
@@ -9,7 +9,7 @@ export default function Img360One() {
             <H2>COOKING SCHOOL</H2>
             <ImgBox>
                 {/* <iframe width="100%" height="600" frameBorder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowFullScreen src="https://kuula.co/share/N8v8G?logo=1&info=1&fs=1&vr=0&sd=1&autorotate=0.27&thumbs=1&alpha=0.60"></iframe>  */}
-                <iframe width="100%" height="640" frameBorder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowFullScreen scrolling="no" src="https://kuula.co/share/N8vgm?logo=1&info=1&fs=1&vr=0&sd=1&autorotate=0.24&thumbs=1"></iframe>           
+                <iframe width="100%" height="640" frameBorder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowFullScreen scrolling="no" src="https://kuula.co/share/N8vgm?logo=1&info=1&fs=1&vr=0&sd=1&autorotate=0.24&thumbs=1" title="kitchen"></iframe>           
             </ImgBox>
         </Container>
     )
@@ -28,8 +28,10 @@ const ImgBox = styled.div`
 `
 
 const H2 = styled.h2`
-/* width: 200px;    */
-    padding-top: 1rem;
+    padding-top: 2rem;
     border-top: 5px solid gray;
-    font-size: 1.8rem;
+    font-size: 2.5rem;
+    font-family: 'Karantina';
+    letter-spacing: 10px;
+    color: gray;
 `
